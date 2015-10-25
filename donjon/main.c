db500c6f7ad80d66d87f527b563a121ea95fd87c
Schmurf <Topicole95>
2015-10-25 20:40:40 +0100

    avancee donjon
    du code tout moche

--- donjon/main.c
+++ donjon/main.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 
 
+
+
+void donjonMap (int *PosX,int *PosY, char tableauMap [20] [20]);
 void murVertical (int x, int y1, int y2, char tableauMap [20] [20]);
 void murHorizontal (int y, int x1, int x2, char tableauMap [20] [20]);
 
-void donjonMap (int *PosX,int *PosY, char tableauMap [20] [20]);
-int deplacement (int *PosX, int *PosY, char tableauMap [20] [20]);
+int inputUtilisateur (int *PosX, int *PosY, int *bourse, char tableauMap [20] [20]);
 void affichage (int *PosX, int *PosY, char tableauMap [20] [20]);
+int combat (int *bourse);
 void Menu ();
 void creationperso ();
 
@@ -17,16 +20,15 @@ int main()
     int fin = 0;
     int PosX = 1;
     int PosY = 1;
-
+    int bourse = 50;
     Menu();
     donjonMap (&PosX,&PosY, tableauMap);
 
     while (fin == 0)
     {
-        fin = deplacement (&PosX,&PosY, tableauMap);
+        fin = inputUtilisateur (&PosX,&PosY, &bourse, tableauMap);
         printf("votre position est %d,%d\n", PosX,PosY);
         affichage (&PosX, &PosY, tableauMap);
-        printf("\ntest");
     }
 
     return 0;
@@ -157,12 +159,52 @@ void affichage (int *PosX, int *PosY, char tableauMap [20] [20])
         printf("\nOu souhaitez vous vous deplacer?\nN/S/W/E\n\n");
 }
 
-int deplacement (int *PosX, int *PosY, char tableauMap [20] [20])
+int menuJeu(int *PosX, int *PosY, int *bourse, char tableauMap [20] [20])
+{
+    int inputMenu;
+    printf("          \nINVENTAIRE\n\n");
+    printf("1. Caracteristiques\n");
+    printf("2. Objets\n");
+    printf("3. Equipement\n");
+    printf("4. Sauvegarder\n");
+    printf("5. Retour en jeu\n");
+    printf("6. Quitter le jeu\n");
+    scanf ("%i", &inputMenu);
+
+    if (inputMenu == 1)
+    {
+        return 0;
+    }
+    if (inputMenu == 2)
+    {
+        return 0;
+    }
+    if (inputMenu == 3)
+    {
+        return 0;
+    }
+
+    if (inputMenu == 4)
+    {
+        return 0;
+    }
+    if (inputMenu == 5)
+    {
+        return 0;
+    }
+
+    if (inputMenu == 6)
+    {
+        return 1;
+    }
+
+}
+int inputUtilisateur (int *PosX, int *PosY, int *bourse, char tableauMap [20] [20])
 {
 
-    char choixDirection;
-    scanf("%c", &choixDirection);
-    if (choixDirection == 'S')
+    char choixUtilisateur;
+    scanf("%c", &choixUtilisateur);
+    if (choixUtilisateur == 'S')
 
     {
         if (tableauMap [*PosY + 1] [*PosX] == 'X')
@@ -171,7 +213,9 @@ int deplacement (int *PosX, int *PosY, char tableauMap [20] [20])
         }
         else if (tableauMap [*PosY + 1] [*PosX] == 'M')
         {
-            combat();
+            combat(bourse);
+            *PosY = *PosY + 1;
+            tableauMap [*PosY - 1] [*PosX] = ' ';
                /* if (return 0)
                 {
                     *PosY = *PosY + 1;
@@ -188,47 +232,85 @@ int deplacement (int *PosX, int *PosY, char tableauMap [20] [20])
             *PosY = *PosY + 1;
             tableauMap [*PosY - 1] [*PosX] = ' ';
         }
-
-
-
     }
 
-    if (choixDirection == 'N')
+    if (choixUtilisateur == 'N')
     {
         if (tableauMap [*PosY - 1] [*PosX] == 'X')
         {
             return 0;
         }
-        *PosY = *PosY - 1;
-        tableauMap [*PosY + 1] [*PosX] = ' ';
+
+        else if (tableauMap [*PosY - 1] [*PosX] == 'M')
+        {
+            combat(bourse);
+            *PosY = *PosY - 1;
+            tableauMap [*PosY + 1] [*PosX] = ' ';
+        }
+        else
+        {
+
+            *PosY = *PosY - 1;
+            tableauMap [*PosY + 1] [*PosX] = ' ';
+        }
+
 
 
     }
 
-    if (choixDirection == 'E')
+    if (choixUtilisateur == 'E')
     {
         if (tableauMap [*PosY] [*PosX + 1] == 'X')
         {
             return 0;
         }
+        else if (tableauMap [*PosY] [*PosX + 1] == 'M')
+        {
+        combat(bourse);
         *PosX = *PosX + 1;
         tableauMap [*PosY] [*PosX - 1] = ' ';
+        }
+        else
+        {
+
+            *PosX = *PosX + 1;
+            tableauMap [*PosY] [*PosX - 1] = ' ';
+        }
+
 
 
     }
 
-    if (choixDirection == 'W')
+    if (choixUtilisateur == 'W')
     {
         if ( tableauMap [*PosY] [*PosX - 1] == 'X')
         {
             return 0;
         }
+
+        else if (tableauMap [*PosY] [*PosX - 1] == 'M')
+        {
+        combat(bourse);
         *PosX = *PosX - 1;
         tableauMap [*PosY] [*PosX + 1] = ' ';
+        }
+        else
+        {
+
+        *PosX = *PosX - 1;
+        tableauMap [*PosY] [*PosX + 1] = ' ';
+        }
+
 
 
     }
-    if (choixDirection == 'F')
+
+    if (choixUtilisateur  == 'I')
+    {
+        system("cls");
+        menuJeu(*PosX, *PosY, *bourse, tableauMap [20] [20]);
+    }
+    if (choixUtilisateur == 'F')
     {
         return 1;
     }
@@ -237,42 +319,71 @@ int deplacement (int *PosX, int *PosY, char tableauMap [20] [20])
 
 }
 
-void feuillePersonnage ()
+/*void feuillePersonnage (int *bourse)
 {
-
+    *bourse = 50;
 }
 
 void feuilleMonstre ()
 {
 
 }
-
-int combat ()
+*/
+int combat (int *bourse)
 {
-    int attaque = 20;
-    int attaqueMonstre = 5;
-    int defenseMonstre = 5;
-    int pvMonstre = 12;
+    int pvChar = 200;
+    int pvCharRestant = pvChar;
+
+    int defense = 10;
+    int attaque = 50;
+    int attaqueMonstre = 25;
+    int defenseMonstre = 10;
+    int pvMonstre = 100;
+    int pvMonstreRestant = pvMonstre;
     int inputCombat;
-    char tableauMap [20] [20];
 
-    printf ("\nVous entrez en combat.\n");
-    printf ("Que souhaitez vous faire?\n");
-    printf ("1. Attaque\n");
-    printf ("2. Inventaire\n");
-    printf ("3. Fuite \n");
-    scanf ("%i", &inputCombat);
-
-    if (inputCombat == 1)
+    while (pvChar | pvMonstre >= 0) // boucle de combat
     {
-        pvMonstre = pvMonstre - attaque;
-    }
-    if (pvMonstre <= 0)
-    {
-        printf("\nLe monstre est mort.\n");
-        tableauMap [4] [4] = ' ';
-        return 0;
 
-    }
+        printf ("\nVous entrez en combat contre un monstre.\n");
+        printf ("Que souhaitez vous faire?\n");
+        printf ("1. Attaque\n");
+        printf ("2. Inventaire\n");
+        printf ("3. Fuite \n");
+        scanf ("%i", &inputCombat);
 
+        if (inputCombat == 1)
+        {
+            pvMonstreRestant = pvMonstre - (attaque - defenseMonstre);
+            pvMonstre = pvMonstreRestant;
+            printf("\nVous attaquez le monstre, il lui reste %i points de vie.", pvMonstreRestant);
+
+            pvCharRestant = pvChar - (attaqueMonstre - defense);
+            pvChar = pvCharRestant;
+            printf ("\nLe monstre vous attaque, il vous reste %i points de vie.\n", pvCharRestant);
+
+        }
+
+        if (inputCombat == 2)
+        {
+            return 0;
+        }
+
+        if (inputCombat == 3)
+        {
+            return 0;
+        }
+        if (pvMonstre <= 0)
+        {
+            printf("\nLe monstre est mort.\n");
+            *bourse = *bourse + 50;
+            return 0;
+        }
+        if (pvChar <= 0)
+        {
+            printf("\nVous etes mort.");
+            system("cls");
+            Menu();
+        }
+    }
 }
